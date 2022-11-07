ffd2ec0ec52960c987eb172f9791bfe6006cb794
RyanCaimano <ryancaimano@gmail.com>
2022-11-07 11:26:18 -0600

    DOES NOT RUN. Forked, cloned, npm installed, but server doesn't like any of the old lock files.

--- User.js
+++ User.js
@@ -2,21 +2,21 @@ const mongoose = require('mongoose');
 const crypto = require('crypto');
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, lowercase: true, unique: true },
-  hash: String,
-  salt: String
+	username: { type: String, lowercase: true, unique: true },
+	hash: String,
+	salt: String
 });
 
 UserSchema.methods.setPassword = function (password) {
-  this.salt = crypto.randomBytes(16).toString('hex');
+	this.salt = crypto.randomBytes(16).toString('hex');
 
-  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+	this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
 };
 
 UserSchema.methods.validPassword = function (password) {
-  const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+	const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
 
-  return this.hash === hash;
+	return this.hash === hash;
 };
 
 const User = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const passport = require('passport');
-const cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session');
 const LocalStrategy = require('passport-local').Strategy;
 
 const User = require('./User');
@@ -15,89 +15,89 @@ app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost/cookie-auth', { useNewUrlParser: true });
 
 app.use(cookieSession({
-  name: 'session',
-  keys: ['helloworld'],
+	name: 'session',
+	keys: ['helloworld'],
 
-  // Cookie Options
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}))
+	// Cookie Options
+	maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.serializeUser((user, done) => {
-  done(null, user._id);
+	done(null, user._id);
 });
 
 passport.deserializeUser((userID, done) => {
-  User.findById(userID, function (err, user) {
-    done(null, user);
-  });
+	User.findById(userID, function (err, user) {
+		done(null, user);
+	});
 });
 
 app.get('/current_user', (req, res) => {
-  res.send(req.user);
+	res.send(req.user);
 });
 
 passport.use('login', new LocalStrategy((username, password, done) => {
-  User.findOne({ username: username }).then(existingUser => {
-    if (existingUser) {
-      // we already have a record with the given profile ID
-      if (existingUser.validPassword(password)) {
-        return done(null, existingUser);
-      } else {
-        return done(false);
-      }
-    } else {
-      const myNewUser = new User({ username: username })
+	User.findOne({ username: username }).then(existingUser => {
+		if (existingUser) {
+			// we already have a record with the given profile ID
+			if (existingUser.validPassword(password)) {
+				return done(null, existingUser);
+			} else {
+				return done(false);
+			}
+		} else {
+			const myNewUser = new User({ username: username });
 
-      myNewUser.setPassword(password);
+			myNewUser.setPassword(password);
 
-      myNewUser.save(function (err, user) {
-        done(null, user);
-      });
-    }
-  })
+			myNewUser.save(function (err, user) {
+				done(null, user);
+			});
+		}
+	});
 }));
 
 const authenticateRequest = function (req, res, next) {
-  if (!req.isAuthenticated()) {// Denied. Redirect to login
-    console.log('DEEEnied')
-    res.redirect('/login');
-  } else {
-    next();
-  }
+	if (!req.isAuthenticated()) {// Denied. Redirect to login
+		console.log('DEEEnied');
+		res.redirect('/login');
+	} else {
+		next();
+	}
 };
 
 app.post('/register', passport.authenticate('login', {
-  successRedirect: '/success',
-  failureRedirect: '/login'
+	successRedirect: '/success',
+	failureRedirect: '/login'
 }));
 
 app.post('/login', passport.authenticate('login', {
-  successRedirect: '/success',
-  failureRedirect: '/login'
+	successRedirect: '/success',
+	failureRedirect: '/login'
 }));
 
 app.get('/logout', (req, res) => {
-  req.logout();
-  res.send('Logged out!');
+	req.logout();
+	res.send('Logged out!');
 });
 
 app.get('/success', (req, res) => {
-  res.send('Hey, hello from the server!');
+	res.send('Hey, hello from the server!');
 });
 
 app.get('/protected', authenticateRequest, (req, res) => {
-  res.send("Secret stuff!");
+	res.send('Secret stuff!');
 });
 
 app.get('/login', (req, res) => {
-  res.sendFile(__dirname + '/login.html');
+	res.sendFile(__dirname + '/login.html');
 });
 
 app.get('/register', (req, res) => {
-  res.sendFile(__dirname + '/register.html');
+	res.sendFile(__dirname + '/register.html');
 });
 
-app.listen(8000)
+app.listen(8000);
